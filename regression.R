3ce82129f244871a1da63e3e2c2991548ab8a10c
Philipp Verpoort <philippv@pik-potsdam.de>
2021-10-24 18:05:02 +0200

    Amendments to the regression analysis.

--- regression.R
+++ regression.R
@@ -8,11 +8,7 @@ library(tidyverse)
 # economics stuff
 i <- 0.08
 n <- 25
-tmp <- 0.0
-for(t in 1:n) {
-    tmp <- tmp + 1.0/(1.0+i)^t
-}
-ANF <- 1.0/tmp
+epsilon <- i*(1+i)^n/((1+i)^n-1)
 
 USD_to_EUR <- 0.86
 
@@ -22,15 +18,15 @@ eff <- 0.7
 
 greenData <- read.csv("./input/regression/regression_data_green.csv")
 
-greenData <- greenData %>% mutate(LCOH =  .[[1]]*USD_to_EUR/33.33,      # USD/kg to EUR/kWh
-                                  p_el =  .[[2]]*USD_to_EUR/1000.0,     # USD/MWh to EUR/kWh
-                                  c_L = .[[3]]*USD_to_EUR,              # USD/kW to EUR/kW
-                                  OCF = .[[4]]
+greenData <- greenData %>% mutate(LCOH  = .[[1]] * USD_to_EUR/33.33,        # USD/kg to EUR/kWh
+                                  p_el  = .[[2]] * USD_to_EUR/1000.0,       # USD/MWh to EUR/kWh
+                                  c_L   = .[[3]] * USD_to_EUR,              # USD/kW to EUR/kW
+                                  OCF   = .[[4]]
                                  ) %>% select(LCOH, p_el, c_L, OCF)
                                         
 print(greenData)
 
-model <- nls(LCOH ~ alpha * ANF*c_L/(8760*(OCF*(1+(theta-1)*log(OCF)))) + gamma * p_el/eff, data=greenData, start=list(alpha=1.0, gamma=1.0, theta=1.0))
+model <- nls(LCOH ~ alpha * epsilon*c_L/(8760*(OCF*(1+(theta-1)*log(OCF)))) + gamma * p_el/eff, data=greenData, start=list(alpha=1.0, gamma=1.0, theta=1.0))
 coefs <- coef(model)
 print(summary(model))
 print(modelr::rsquare(model, greenData))
@@ -42,11 +38,7 @@ print(modelr::rsquare(model, greenData))
 # economics stuff
 i <- 0.08
 n <- 25
-tmp <- 0.0
-for(t in 1:n) {
-    tmp <- tmp + 1.0/(1.0+i)^t
-}
-ANF <- 1.0/tmp
+epsilon <- i*(1+i)^n/((1+i)^n-1)
 
 # physics stuff
 ci_ng <- 0.198 # CI of NG: tCO2eq/kWh
@@ -58,20 +50,21 @@ cr <- c(smr = 0.0, heb = 0.60, leb = 0.90)
 for(type in c('smr', 'heb', 'leb')) {
     blueData <- read.csv(paste0('./input/regression/regression_data_blue_', type, '.csv'))
 
-    blueData <- blueData %>% mutate(LCOH =  .[[1]],                 # EUR/kWh
-                                    p_ng =  .[[2]]*3.6/1000.0,      # EUR/GJ to EUR/kWh
+    blueData <- blueData %>% mutate(LCOH  = .[[1]],                 # EUR/kWh
+                                    p_ng  = .[[2]] * 3.6/1000.0,    # EUR/GJ to EUR/kWh
                                     c_CTS = .[[3]],                 # EUR/tCO2
-                                    c_CAP = .[[4]]*10.0/3.0         # MEUR/(10^5Nm³/h) to EUR/kW
-                                   ) %>% select(LCOH, p_ng, c_CTS, c_CAP)
+                                    C_pl  = .[[4]] * 10^6,          # MEUR to EUR
+                                    P_pl  = 1 * 3*10^5              # Nm3/h to kW
+                                   ) %>% select(LCOH, p_ng, c_CTS, C_pl, P_pl)
                                          
     print(blueData)
 
     if(type == 'smr') {
-        model <- nls(LCOH ~ alpha * ANF*c_CAP/8760 + gamma * p_ng/eff[type], data=blueData, start=list(alpha=1.0, gamma=1.0))
+        model <- nls(LCOH ~ alpha * epsilon*C_pl/(P_pl*8760) + gamma * p_ng/eff[type], data=blueData, start=list(alpha=1.0, gamma=1.0))
     }
     else {
-        #model<-nls(LCOH ~ alpha * ANF*c_CAP/8760 + gamma * p_ng/eff[type] + phi * cr[type]*ci_ng*c_CTS/eff[type], data=blueData, start=list(alpha=1.0, gamma=1.0, phi=1.0))
-        model <- nls(LCOH ~ alpha * ANF*c_CAP/8760 + gamma * p_ng/eff[type] + phi * ci_ng*c_CTS, data=blueData, start=list(alpha=1.0, gamma=1.0, phi=1.0))
+        model <- nls(LCOH ~ alpha * epsilon*C_pl/(P_pl*8760) + gamma * p_ng/eff[type] + mu * cr[type]*ci_ng*c_CTS/eff[type], data=blueData, start=list(alpha=1.0, gamma=1.0, mu=1.0))
+        #model <- nls(LCOH ~ alpha * epsilon*C_pl/8760 + gamma * p_ng/eff[type] + mu * ci_ng*c_CTS, data=blueData, start=list(alpha=1.0, gamma=1.0, mu=1.0))
     }
     coefs <- coef(model)
     print(summary(model))
