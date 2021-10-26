012cf989aebef04c603a9a21a8b2063337b947c7
Philipp Verpoort <philippv@pik-potsdam.de>
2021-10-26 17:31:52 +0200

    Some changes on the regression data.

--- regression.R
+++ regression.R
@@ -1,8 +1,6 @@
 library(dplyr)
 
-
-
-###### Green Data Regression Analysis  ######
+###### Green Data Regression Analysis ######
 
 # economics stuff
 i <- 0.08
@@ -12,9 +10,9 @@ epsilon <- i*(1+i)^n/((1+i)^n-1)
 USD_to_EUR <- 0.86
 
 # physics stuff
-ci_ng <- 0.198 # CI of NG: tCO2eq/kWh
-eff <- 0.7
+eff <- 0.7 # electrolysis efficiency of ~70%
 
+# regression analysis
 greenData <- read.csv("./input/regression/regression_data_green.csv")
 
 greenData <- greenData %>% mutate(LCOH  = .[[1]] * USD_to_EUR/33.33,        # USD/kg to EUR/kWh
@@ -24,13 +22,12 @@ greenData <- greenData %>% mutate(LCOH  = .[[1]] * USD_to_EUR/33.33,        # US
                                  ) %>% select(LCOH, p_el, c_L, OCF)
 
 model <- nls(LCOH ~ alpha * epsilon*c_L/(8760*(OCF*(1+(theta-1)*log(OCF)))) + gamma * p_el/eff, data=greenData, start=list(alpha=1.0, gamma=1.0, theta=1.0))
-coefs <- coef(model)
 print(summary(model))
 print(modelr::rsquare(model, greenData))
 
 
 
-###### Blue Data Regression Analysis  ######
+###### Blue Data Regression Analysis ######
 
 # economics stuff
 i <- 0.08
@@ -40,10 +37,7 @@ epsilon <- i*(1+i)^n/((1+i)^n-1)
 # physics stuff
 ci_ng <- 198/10^6 # CI of NG: 55 kgCO2eq/GJ = 198 gCO2eq/kWh = 198e-6 tCO2eq/kWh
 
-# variables of different production types
-eff <- c(smr = 0.7, heb = 0.6, leb = 0.5)
-cr <- c(smr = 0.0, heb = 0.60, leb = 0.90)
-
+# regression analysis
 for(type in c('smr', 'heb', 'leb')) {
     blueData <- read.csv(paste0('./input/regression/regression_data_blue_', type, '.csv'))
 
@@ -51,16 +45,24 @@ for(type in c('smr', 'heb', 'leb')) {
                                     p_ng  = .[[2]] * 3.6/1000.0,    # EUR/GJ to EUR/kWh
                                     c_CTS = .[[3]],                 # EUR/tCO2
                                     C_pl  = .[[4]] * 10^6,          # MEUR to EUR
+                                    emi   = .[[5]] * 1.0/3000.0,    # kgCO2/Nm³_H2 to tCO2eq/kWh_H2-LHV
                                     P_pl  = 1 * 3*10^5              # Nm3/h to kW
-                                   ) %>% select(LCOH, p_ng, c_CTS, C_pl, P_pl)
+                                   ) %>% select(LCOH, p_ng, c_CTS, C_pl, emi, P_pl, eff, CR)
 
     if(type == 'smr') {
-        model <- nls(LCOH ~ alpha * epsilon*C_pl/(P_pl*8760) + gamma * p_ng/eff[type], data=blueData, start=list(alpha=1.0, gamma=1.0))
+        model <- nls(LCOH ~ alpha * epsilon*C_pl/(P_pl*8760) + gamma * p_ng/eff, data=blueData, start=list(alpha=1.0, gamma=1.0))
+        print(summary(model))
+        print(modelr::rsquare(model, blueData))
     }
     else {
-        model <- nls(LCOH ~ alpha * epsilon*C_pl/(P_pl*8760) + gamma * p_ng/eff[type] + mu * cr[type]*ci_ng*c_CTS/eff[type], data=blueData, start=list(alpha=1.0, gamma=1.0, mu=1.0))
+        # using official capture rate from report
+        model <- nls(LCOH ~ alpha * epsilon*C_pl/(P_pl*8760) + gamma * p_ng/eff + mu * c_CTS*emi, data=blueData, start=list(alpha=1.0, gamma=1.0, mu=1.0))
+        print(summary(model))
+        print(modelr::rsquare(model, blueData))
+        
+        # using own computed capture rate
+        model <- nls(LCOH ~ alpha * epsilon*C_pl/(P_pl*8760) + gamma * p_ng/eff + mu * CR*ci_ng*c_CTS/eff, data=blueData, start=list(alpha=1.0, gamma=1.0, mu=1.0))
+        print(summary(model))
+        print(modelr::rsquare(model, blueData))
     }
-    coefs <- coef(model)
-    print(summary(model))
-    print(modelr::rsquare(model, blueData))
 }
