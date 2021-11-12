1f8a9ebed5c033c1aa6ba0ebbed764e6ecd8bbe4
Philipp Verpoort <philippv@pik-potsdam.de>
2021-11-12 21:39:09 +0100

    Updated regression data and script.

--- regression.R
+++ regression.R
@@ -36,32 +36,29 @@ epsilon <- i*(1+i)^n/((1+i)^n-1)
 
 # physics stuff
 ci_ng <- 198/10^6 # CI of NG: 55 kgCO2eq/GJ = 198 gCO2eq/kWh = 198e-6 tCO2eq/kWh
+ohrs <- 8322.0
 
 # regression analysis
 for(type in c('smr', 'heb', 'leb')) {
     blueData <- read.csv(paste0('./input/regression/regression_data_blue_', type, '.csv'))
 
-    blueData <- blueData %>% mutate(LCOH  = .[[1]],                 # EUR/kWh
-                                    p_ng  = .[[2]] * 3.6/1000.0,    # EUR/GJ to EUR/kWh
-                                    c_CTS = .[[3]],                 # EUR/tCO2
-                                    C_pl  = .[[4]] * 10^6,          # MEUR to EUR
-                                    emi   = .[[5]] * 1.0/3000.0,    # kgCO2/Nm³_H2 to tCO2eq/kWh_H2-LHV
-                                    P_pl  = 1 * 3*10^5              # Nm3/h to kW
-                                   ) %>% select(LCOH, p_ng, c_CTS, C_pl, emi, P_pl, eff, CR)
+    blueData <- blueData %>% mutate(LCOH   = .[[1]]/3.0,                    # EUR/Nm³_H2 to EUR/kWh_LHV
+                                    p_ng   = .[[2]] * 3.6/1000.0,           # EUR/GJ to EUR/kWh
+                                    c_CTS  = .[[3]],                        # EUR/tCO2
+                                    C_pl   = .[[4]] * 10^6,                 # MEUR to EUR
+                                    emi    = .[[5]] * 1.0/3000.0,           # kgCO2/Nm³_H2 to tCO2eq/kWh_H2-LHV
+                                    fOnM_pl= .[[8]] * 10^6,                 # MEUR to EUR
+                                    vOnM_pl= .[[9]] * 10^6,                 # MEUR to EUR
+                                    P_pl   = 1 * 3*10^5                     # Nm3/h to kW
+                                   ) %>% select(LCOH, p_ng, c_CTS, C_pl, emi, fOnM_pl, vOnM_pl, P_pl, eff, CR)
 
     if(type == 'smr') {
-        model <- nls(LCOH ~ alpha * epsilon*C_pl/(P_pl*8760) + gamma * p_ng/eff, data=blueData, start=list(alpha=1.0, gamma=1.0))
+        model <- nls(LCOH ~ alpha * (epsilon*C_pl+fOnM_pl+vOnM_pl)/(P_pl*ohrs) + gamma * p_ng/eff, data=blueData, start=list(alpha=1.0, gamma=1.0))
         print(summary(model))
         print(modelr::rsquare(model, blueData))
     }
     else {
-        # using official capture rate from report
-        model <- nls(LCOH ~ alpha * epsilon*C_pl/(P_pl*8760) + gamma * p_ng/eff + mu * c_CTS*emi, data=blueData, start=list(alpha=1.0, gamma=1.0, mu=1.0))
-        print(summary(model))
-        print(modelr::rsquare(model, blueData))
-        
-        # using own computed capture rate
-        model <- nls(LCOH ~ alpha * epsilon*C_pl/(P_pl*8760) + gamma * p_ng/eff + mu * CR*ci_ng*c_CTS/eff, data=blueData, start=list(alpha=1.0, gamma=1.0, mu=1.0))
+        model <- nls(LCOH ~ alpha * (epsilon*C_pl+fOnM_pl+vOnM_pl)/(P_pl*ohrs) + gamma * p_ng/eff + mu * c_CTS*emi, data=blueData, start=list(alpha=1.0, gamma=1.0, mu=1.0))
         print(summary(model))
         print(modelr::rsquare(model, blueData))
     }
