64019e58d5e6d80b4662ae176f2ba04de75b5565
Philipp Verpoort <philippv@pik-potsdam.de>
2022-05-18 14:25:38 +0200

    Minor code refactoring.

--- src/data/data.py
+++ src/data/data.py
@@ -1,7 +1,7 @@
 import pandas as pd
 
 from src.config_load import units
-from src.filepaths import getFilePath
+from src.filepaths import getFilePathOutput
 from src.data.params.full_params import getFullParams
 from src.data.FSCPs.calc_FSCPs import calcFSCPs
 from src.data.fuels.calc_fuels import calcFuelData
@@ -9,7 +9,7 @@ from src.data.steel.calc_steel import calcSteelData
 
 
 # obtain all required data for a scenario
-def getFullData(input_data: dict, steel_data: dict, export_data: bool = True):
+def getFullData(input_data: dict, steel_data: dict):
     options, params, fuels = (input_data['options'], input_data['params'], input_data['fuels'])
 
     # convert basic inputs to complete dataframes
@@ -29,22 +29,24 @@ def getFullData(input_data: dict, steel_data: dict, export_data: bool = True):
     FSCPData = calcFSCPs(fuelData)
     FSCPDataSteel = calcFSCPs(fuelDataSteel)
 
-    # dump to output file
-    if export_data:
-        print('Exporting...')
-        filePath = getFilePath('output/', 'data.xlsx')
-        writer = pd.ExcelWriter(filePath)
-
-        columnOrder = ['description', 'type', 'value', 'unit', 'source']
-
-        pd.DataFrame(params).T.reindex(columnOrder, axis=1).to_excel(writer, sheet_name='Parameters (input)')
-        pd.DataFrame(fuels).T.to_excel(writer, sheet_name='Fuel list (input)')
-
-        fullParams.to_excel(writer, sheet_name='Parameters (full)')
-        fuelData.to_excel(writer, sheet_name='Fuel data (output)')
-
-        writer.save()
+    # export selected input and output data as XLS
+    exportDataXLS(params, fuels, fullParams, fuelData)
 
     return fullParams, fuelSpecs, fuelData, FSCPData, fuelDataSteel, FSCPDataSteel
 
 
+# export selected data into XLS output file
+def exportDataXLS(params, fuels, fullParams, fuelData):
+    print('Exporting...')
+    filePath = getFilePathOutput('data.xlsx')
+    writer = pd.ExcelWriter(filePath)
+
+    columnOrder = ['description', 'type', 'value', 'unit', 'source']
+
+    pd.DataFrame(params).T.reindex(columnOrder, axis=1).to_excel(writer, sheet_name='Parameters (input)')
+    pd.DataFrame(fuels).T.to_excel(writer, sheet_name='Fuel list (input)')
+
+    fullParams.to_excel(writer, sheet_name='Parameters (full)')
+    fuelData.to_excel(writer, sheet_name='Fuel data (output)')
+
+    writer.save()
